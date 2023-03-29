5c4c91bae775e0e8dd1f18993a8de50772ef10a1
Mark Sherman <shermanm@emmanuel.edu>
2023-03-29 12:37:13 -0400

    Inheritance example: ParaRock
    Making a subclass of the Rock to have a (primitive) parachute. From class on Tuesday, March  28.
    
    Moves the control logic into an update() function so that it can be inherited, without needing to re-write it. Draw now simply draws, and calls update.

--- sketch.js
+++ sketch.js
@@ -5,12 +5,28 @@ class Rock {
     this.speed = speed;
   }
 
-  draw () {
-    ellipse(this.x, height - this.altitude, 10);
+  update() {
     this.altitude -= this.speed;
     if(this.altitude <= 4) {
       this.altitude = 4;
     }
+  }
+
+  draw () {
+    ellipse(this.x, height - this.altitude, 10);
+    this.update();
+  }
+}
+
+class ParaRock extends Rock {
+  constructor(x, altitude, speed){
+    super(x, altitude, speed);
+  }
+  draw () {
+    ellipse(this.x, height - this.altitude - 10, 30);
+    ellipse(this.x, height - this.altitude, 10);
+    text('overload', this.x, height - this.altitude + 10)
+    this.update();
   }
 }
 
@@ -20,7 +36,7 @@ let leftRock;
 function setup () {
   createCanvas(300, 300);
   myRock = new Rock(width / 2, height, 1);
-  leftRock = new Rock(100, height, 0.9);
+  leftRock = new ParaRock(100, height, 0.9);
 }
 
 function draw () {
